2c0faa5f7783ca1933667985032db45e88798977
Omer ilmaz <omery020040@gmail.com>
2023-03-06 11:04:31 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,20 +32,20 @@ import TestimonialSlider from "../components/UI/slider/TestimonialSlider.js";
 
 const featureData = [
   {
-    title: "Quick Delivery",
+    title: "Hızlı sipariş",
     imgUrl: featureImg01,
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    desc: "Sipariş sırası beklemeden siparişini ver ve dakikalar içinde siparişin masanda olsun",
   },
 
   {
-    title: "Super Dine In",
+    title: "Kolay Sipariş",
     imgUrl: featureImg02,
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    desc: "Qr kodu okut ve menüden dilediğini seç",
   },
   {
-    title: "Easy Pick Up",
+    title: "Kolay Ödeme",
     imgUrl: featureImg03,
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    desc: "Kasaya Gitmeden ödemenizi istediğiniz yerden yapabalirsiniz",
   },
 ];
 
@@ -99,43 +99,39 @@ const Home = () => {
             <Col lg="6" md="6">
               <div className="hero__content  ">
                 <h1 className="mb-4 hero__title">
-                  <span>Bi tıkla Sipariş</span>te <br /> sipariş vermek hiç bu kadar kolay olmamıştı
+                  <span>Bi tıkla Sipariş</span>te <br /> sipariş vermek hiç bu
+                  kadar kolay olmamıştı
                   {/* <span>Bi tıkla Sipariş</span> ile <br /> artık siparişler bi' tık uzağınızda */}
-                  <p>İstediğiniz zaman garsonsuz sipariş verebilir</p>
+                  <p>
+                    Artık Qr kod ile menüye istediğiniz yerden ulaşabilir ve bir
+                    tıkla siparişinizi vererek garson ya da kasa da sıra
+                    beklemenize gerek duymayacaksınız
+                  </p>
                 </h1>
-
-                <p>
-                  
-                </p>
-
                 <div className="hero__btns d-flex align-items-center gap-5 mt-4">
                   <button className="order__btn d-flex align-items-center justify-content-between">
                     Order now <i class="ri-arrow-right-s-line"></i>
                   </button>
-
                   <button className="all__foods-btn">
                     <Link to="/foods">See all foods</Link>
                   </button>
                 </div>
-
                 <div className=" hero__service  d-flex align-items-center gap-5 mt-5 ">
                   <p className=" d-flex align-items-center gap-2 ">
                     <span className="shipping__icon">
                       <i class="ri-car-line"></i>
-                    </span>{" "}
+                    </span>
                     No shipping charge
                   </p>
-
                   <p className=" d-flex align-items-center gap-2 ">
                     <span className="shipping__icon">
                       <i class="ri-shield-check-line"></i>
-                    </span>{" "}
+                    </span>
                     100% secure checkout
                   </p>
                 </div>
               </div>
             </Col>
-
             <Col lg="6" md="6">
               <div className="hero__img">
                 <img src={heroImg} alt="hero-img" className="w-100" />
@@ -144,27 +140,18 @@ const Home = () => {
           </Row>
         </Container>
       </section>
-
-      <section className="pt-0">
-        <Category />
-      </section>
-
       <section>
         <Container>
           <Row>
             <Col lg="12" className="text-center">
-              <h5 className="feature__subtitle mb-4">What we serve</h5>
-              <h2 className="feature__title">Just sit back at home</h2>
+              <h5 className="feature__subtitle mb-4">Ne Sunuyoruz</h5>
               <h2 className="feature__title">
-                we will <span>take care</span>
+                Masanda oturup <span>bir tıkla sipariş</span> verebilmeni
               </h2>
-              <p className="mb-1 mt-4 feature__text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-                officiis?
-              </p>
+              <p className="mb-1 mt-4 feature__text"></p>
               <p className="feature__text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Aperiam, eius.{" "}
+                Masanda Otur ve Qr kodu okutarak istediğin ürünü sepete ekle ve
+                dilediğince sipariş ver
               </p>
             </Col>
 
@@ -243,7 +230,7 @@ const Home = () => {
         </Container>
       </section> */}
 
-      <section className="why__choose-us">
+      {/* <section className="why__choose-us">
         <Container>
           <Row>
             <Col lg="6" md="6">
@@ -253,20 +240,12 @@ const Home = () => {
             <Col lg="6" md="6">
               <div className="why__tasty-treat">
                 <h2 className="tasty__treat-title mb-4">
-                  Why <span>Tasty Treat?</span>
+                  Neden <span>Biz </span>
                 </h2>
-                <p className="tasty__treat-desc">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dolorum, minus. Tempora reprehenderit a corporis velit,
-                  laboriosam vitae ullam, repellat illo sequi odio esse iste
-                  fugiat dolor, optio incidunt eligendi deleniti!
-                </p>
-
                 <ListGroup className="mt-4">
                   <ListGroupItem className="border-0 ps-0">
                     <p className=" choose__us-title d-flex align-items-center gap-2 ">
-                      <i class="ri-checkbox-circle-line"></i> Fresh and tasty
-                      foods
+                      <i class="ri-checkbox-circle-line"></i> Hızlı Sipariş
                     </p>
                     <p className="choose__us-desc">
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -299,7 +278,7 @@ const Home = () => {
             </Col>
           </Row>
         </Container>
-      </section>
+      </section> */}
 
       {/* <section className="pt-0">
         <Container>
@@ -317,7 +296,7 @@ const Home = () => {
         </Container>
       </section> */}
 
-      <section>
+      {/* <section>
         <Container>
           <Row>
             <Col lg="6" md="6">
@@ -341,7 +320,7 @@ const Home = () => {
             </Col>
           </Row>
         </Container>
-      </section>
+      </section> */}
     </Helmet>
   );
 };
